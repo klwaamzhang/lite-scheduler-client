01e5ec111618de80dfa1b8e2fe82bbd112583a74
Yizhi Zhang <36008792+klwaamzhang@users.noreply.github.com>
2020-07-17 20:14:35 -0400

    added docs

---
+++ docs/precache-manifest.9d109092010d4d966d7cd374eea4bbd5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f300f41d45c5cbd3cc5f4c085a98d6d",
+    "url": "/lite-scheduler-client/index.html"
+  },
+  {
+    "revision": "9e2fbc517a7a3c7430d0",
+    "url": "/lite-scheduler-client/static/js/2.96921f4c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/lite-scheduler-client/static/js/2.96921f4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1ce5b95e250632f9326",
+    "url": "/lite-scheduler-client/static/js/main.f63a1819.chunk.js"
+  },
+  {
+    "revision": "a69eafa6231be008cc48",
+    "url": "/lite-scheduler-client/static/js/runtime-main.276e641e.js"
+  }
+]);
